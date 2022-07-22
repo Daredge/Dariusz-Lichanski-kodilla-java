9680103fbcf617e45d65310e09147344d785313b
Dariusz Lichański <lichanskidariusz@gmail.com>
2022-07-22 12:16:59 +0200

    Added SimpleUser, Calculaotr classes

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,36 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+
+        // first test
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        // second test
+        Calculator calculator = new Calculator();
+
+        if (calculator.add(5, 6) == 11) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        // third test
+        if (calculator.subtract(5, 6) == -1) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
